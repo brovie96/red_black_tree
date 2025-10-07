15c5df076adcc3dd94f966ca3aa7076156e6a7b1
Seth Riedel <seth.riedel@gmail.com>
2025-10-07 16:25:13 -0700

    Add sync version and docs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use red_black_tree::{rbt, RedBlackTree};
+use std::sync::{Arc, Mutex};
+use rayon::prelude::*;
+use red_black_tree::sync::RedBlackTree;
 
 fn main() {
-    let tree = rbt!(i in 1..=15);
+    let tree = Arc::new(Mutex::new(RedBlackTree::new()));
+
+    (1..=30).into_par_iter().for_each(|i| {
+        tree.lock().unwrap().insert(i);
+    });
+
     println!("{tree:?}");
 }
